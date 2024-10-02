6648322108662fffe68d01fbd8509f4a731a7cd4
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-02 15:31:35 +0200

    added form

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
-<?php 
+<!DOCTYPE html>
+<html lang="it">
 
- echo 'PHP';
-?>
-
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
 
+    <!--Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-  <h1>HTML</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>BadWords</h1>
+                <form action="./bad.php" method="get">
+                    <div class="mb-3">
+                        <label for="UserText" class="form-label">Type a text</label>
+                        <input type="text" class="form-control" id="user-text" name="UserText" placeholder="Give papa a cup of proper coffe in a copper coffe cup">
+                        <div id="emailHelp" class="form-text">We'll never share your text with anyone else.</div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
