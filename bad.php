033cbede0750eedbf38c937d2c81317a8cb28dd9
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-02 15:39:13 +0200

    get text

---
+++ bad.php
@@ -0,0 +1,28 @@
+<?php
+//GET text from form
+$text = $_GET["UserText"];
+?>
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badword</title>
+
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>BadWords</h1>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
